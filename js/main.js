dd68b0179320219b043a5ec3176dea1f8cad2947
Andrea Genovese <andregenovese2000@gmail.com>
2022-07-06 16:17:30 +0200

    parte finale

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 const containerDom = document.getElementById('container');
 
 for (let i = 1; i <= 100; i++) {
-    containerDom.innerHTML += `<div class="box">${i}</div>`;
+    let contenuto = i;
+    let classe = "";
+    containerDom.innerHTML += `<div class="box">${contenuto}</div>`;
+
+    if (i % 15== 0) 
+     console.log("FizzBuzz");
+    else if (i % 3 == 0)
+     console.log("Fizz");
+    else if (i % 5 == 0)
+     console.log("Buzz");
+    else
+     console.log(i);
+
+
+
+
+    
 }
+
+
 
